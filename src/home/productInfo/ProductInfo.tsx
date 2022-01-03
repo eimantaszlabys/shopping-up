da6ee4db4ae95c40abe61e23ce3cb11290a0dda0
Eimantas Žlabys <eimantas.zlabys@gmail.com>
2022-01-03 16:19:21 +0200

    Initial products

--- src/home/productInfo/ProductInfo.tsx
+++ src/home/productInfo/ProductInfo.tsx
@@ -1,5 +1,15 @@
 import React, { FC } from 'react';
 import { ProductInfoProps } from './types';
 
-const ProductInfo: FC<ProductInfoProps> = () => <div>Product</div>;
+const ProductInfo: FC<ProductInfoProps> = ({
+  title, description, filename, price, rating,
+}) => (
+  <div>
+    <img src={`/products/${filename}`} alt="" />
+    <h2>{title}</h2>
+    <p>{description}</p>
+    <b>{price}</b>
+    <div>{rating}</div>
+  </div>
+);
 export { ProductInfo };
