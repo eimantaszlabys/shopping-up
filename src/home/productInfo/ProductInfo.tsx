729951a31087063f28f4e1215ae7f793173492c8
Eimantas Žlabys <eimantas.zlabys@gmail.com>
2022-01-03 17:05:47 +0200

    Stile product item

--- src/home/productInfo/ProductInfo.tsx
+++ src/home/productInfo/ProductInfo.tsx
@@ -1,15 +1,19 @@
 import React, { FC } from 'react';
+import classnames from 'classnames';
 import { ProductInfoProps } from './types';
+import styles from './styles.module.css';
 
 const ProductInfo: FC<ProductInfoProps> = ({
-  title, description, filename, price, rating,
+  title, description, filename, price,
 }) => (
-  <div>
-    <img src={`/products/${filename}`} alt="" />
-    <h2>{title}</h2>
-    <p>{description}</p>
-    <b>{price}</b>
-    <div>{rating}</div>
+  <div className={classnames(styles.card, styles.stacked)}>
+    <img className={styles.img} src={`/products/${filename}`} alt={title} />
+    <div className={styles.content}>
+      <h2 className={styles.title}>{title}</h2>
+      <p className={styles.price}>{price}</p>
+      <p className={styles.description}>{description}</p>
+
+    </div>
   </div>
 );
 export { ProductInfo };
