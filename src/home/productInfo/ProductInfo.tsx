1ec1ed66fdbad678f88807853ea2167e93baf0cc
Eimantas Žlabys <eimantas.zlabys@gmail.com>
2022-01-03 19:42:01 +0200

    Format price

--- src/home/productInfo/ProductInfo.tsx
+++ src/home/productInfo/ProductInfo.tsx
@@ -5,15 +5,22 @@ import styles from './styles.module.css';
 
 const ProductInfo: FC<ProductInfoProps> = ({
   title, description, filename, price,
-}) => (
-  <div className={classnames(styles.card, styles.stacked)}>
-    <img className={styles.img} src={`/products/${filename}`} alt={title} />
-    <div className={styles.content}>
-      <h2 className={styles.title}>{title}</h2>
-      <p className={styles.price}>{price}</p>
-      <p className={styles.description}>{description}</p>
+}) => {
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'EUR',
+  });
 
+  return (
+    <div className={classnames(styles.card, styles.stacked)}>
+      <img className={styles.img} src={`/products/${filename}`} alt={title} />
+      <div className={styles.content}>
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.price}>{formatter.format(price)}</p>
+        <p className={styles.description}>{description}</p>
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
 export { ProductInfo };
