d1e64bda1968744a853c8e5a8fc2f8b9ac99d6ab
Eimantas Žlabys <eimantas.zlabys@gmail.com>
2022-02-08 14:07:32 +0200

    Added sections

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -2,15 +2,53 @@ import React, { FunctionComponent } from 'react';
 import styles from './styles.module.scss';
 
 const Home: FunctionComponent = () => {
+  const sections = [
+    {
+      image: 'Image',
+      title: 'Copywriting',
+      explanatory:
+        'Sample text. Click to select the text box. Click again or double click to start editing the text.'
+    },
+    {
+      image: 'Image',
+      title: 'Graphic Design',
+      explanatory:
+        'Sample text. Click to select the text box. Click again or double click to start editing the text.'
+    },
+    {
+      image: 'Image',
+      title: 'Development',
+      explanatory:
+        'Sample text. Click to select the text box. Click again or double click to start editing the text.'
+    },
+    {
+      image: 'Image',
+      title: 'Logistics',
+      explanatory:
+        'Sample text. Click to select the text box. Click again or double click to start editing the text.'
+    }
+  ];
+
   return (
     <>
-      <div className={styles.menu}>
-        <button>Menu</button>
+      <div>
+        <button type="button">Menu</button>
       </div>
       <div className={styles.content}>
         <h1>IT Services</h1>
         <h3>We Provide Outsourced</h3>
         <h3>for Small & Mid-sized Businesses</h3>
+      </div>
+      <div className={styles.sections}>
+        {sections.map(({ image, title, explanatory }) => (
+          <div className={styles.section}>
+            <div>{image}</div>
+            <div className={styles.info}>
+              <h1>{title}</h1>
+              <span>{explanatory}</span>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
