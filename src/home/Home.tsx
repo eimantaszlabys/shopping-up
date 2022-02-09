8f7c060d55a2ca2920dce03f13bc92238f564dc0
Eimantas Žlabys <eimantas.zlabys@gmail.com>
2022-02-09 16:31:21 +0200

    Added styles

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -5,20 +5,51 @@ import image1 from './images/10084175.jpeg';
 import image2 from './images/10211524.jpeg';
 import image3 from './images/9852577.jpeg';
 import image4 from './images/9489047.jpeg';
+import image5 from './images/10161997.jpeg';
 
 const Home: FunctionComponent = () => {
   return (
-    <div>
+    <>
       <h3 className={styles.subHeader}>Fashion house</h3>
       <h1 className={styles.header}>Fashion</h1>
 
-      <div className={classnames(styles.grid, styles.gridSection)}>
+      <div className={classnames(styles.part1gridSection, styles.part1grid)}>
         <img className={styles.image1} src={image1} alt="image1" />
         <img className={styles.image2} src={image2} alt="image2" />
         <img className={styles.image3} src={image3} alt="image3" />
         <img className={styles.image4} src={image4} alt="image4" />
       </div>
-    </div>
+
+      <div className={classnames(styles.part2gridSection, styles.part2grid)}>
+        <img className={styles.image5} src={image5} alt="image5" />
+        <h2 className={styles.header}>
+          ​We collaborate with brands and agencies to create memorable experiences.
+        </h2>
+        <div className={styles.infoSection}>
+          <div className={styles.info}>
+            <p>
+              Sample text. Click to select the text box. Click again or double click to start
+              editing the text.
+            </p>
+            <div className={styles.sectionNumber}>01</div>
+          </div>
+          <div className={styles.info}>
+            <p>
+              ​The principal reason we continue to adapt and evolve our business model is to ensure
+              that we are meeting our customers’ expectations. One example of this has been to use
+              modern technology and the introduction of the real time tracking our teams using GPS. 
+            </p>
+          </div>
+          <div className={styles.info}>
+            <p>
+              Sample text. Click to select the text box. Click again or double click to start
+              editing the text.
+            </p>
+            <div className={styles.sectionNumber}>02</div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
